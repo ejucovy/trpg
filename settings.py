a53f970e1fa7333f0f5daa43bde865e9b143cdc4
Ethan Jucovy <ethan.jucovy+github@gmail.com>
2011-06-06 11:21:32 -0400

    debug logging

--- settings.py
+++ settings.py
@@ -74,6 +74,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'bagels.main.middleware.ConsoleExceptionMiddleware',
 )
 
 ROOT_URLCONF = 'bagels.urls'
